dae404669805b9b704b05f581a929afc749135ea
Tristan Emmerson <tristan@stickerslug.com>
2020-07-03 15:08:30 -0700

    add currencyType argument.

--- src/getRates.js
+++ src/getRates.js
@@ -1,6 +1,8 @@
-export async function getRates() {
+export async function getRates(currencyType) {
   try {
-    let response = await fetch(`https://v6.exchangerate-api.com/v6/${[process.env.API_KEY]}/latest/USD`);
+    console.log(currencyType);
+    console.log(currencyType);
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${[process.env.API_KEY]}/latest/${currencyType}`);
     let jsonifiedResponse;
     if (response.ok && response.status == 200) {
       jsonifiedResponse = await response.json();
