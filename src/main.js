902e2b1325f54fc50f07eb6f1de3cee7ff3da499
Tristan Emmerson <tristan@stickerslug.com>
2020-07-03 10:34:52 -0700

    refactor tags and import headers.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-import $ from 'jQuery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { getRates } from './getRates';
+import { getRate } from './getRates';
 
+async function getRates(USD) {
+  const jsonifiedResponse = await getRate(USD);
+  if (jsonifiedResponse === false) {
+    $("#exchangeValue").text("I'm sorry, something went wrong with your request")
+  } else {
+    $("#exchangeValue").text(`Exchange Value: ${jsonifiedResponse.USD}`);
+  }
+}
 
 $(document).ready(function() {
-  $('#rateInfo').click(function(event) {
-    event.preventDefault();
+  $("#exchangeValue").click(function() {
+    let USD = $("#conversion_rates").val();
+    $("#conversion_rates").val("");
+
+    getRates(USD);
   });
-});
+});  
