b313fd289b5af3a4740e781121ec1dece9003794
Tristan Emmerson <tristan@stickerslug.com>
2020-07-03 15:12:44 -0700

    add console logging.

--- src/getRates.js
+++ src/getRates.js
@@ -1,8 +1,8 @@
 export async function getRates(currencyType) {
   try {
     console.log(currencyType);
-    console.log(currencyType);
-    let response = await fetch(`https://v6.exchangerate-api.com/v6/${[process.env.API_KEY]}/latest/${currencyType}`);
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${[process.env.API_KEY]}/latest/USD`);
+    console.log(response);
     let jsonifiedResponse;
     if (response.ok && response.status == 200) {
       jsonifiedResponse = await response.json();
